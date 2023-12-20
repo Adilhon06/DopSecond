6dd45da11d14757c1a6c8254559436ef3ea15779
Adilhon06 <adubaichik2006@gmail.com>
2023-12-20 17:51:37 +0600

    changed

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,26 @@ public class Main {
 
         int age = getRandomAge();
 
-        getBack(getRandomAge(), 20);
-        getBack(getRandomAge(), 10);
-        getBack(getRandomAge(), -20);
-        getBack(getRandomAge(), 16);
-        getBack(getRandomAge(), 36);
+        System.out.println(getBack(age, 20));
+        System.out.println(getBack(age, 10));
+        System.out.println(getBack(age, -20));
+        System.out.println(getBack(age, 16));
+        System.out.println(getBack(age, 36));
     }
     public static int getRandomAge(){
         Random random = new Random();
-        int randomAge = random.nextInt(80) + 1;
-        return randomAge;
+        return random.nextInt(100) + 1;
     }
     public static String getBack(int age, int temperature){
 
-        if (age >= 20 && age <=40 && temperature >= -20 && temperature <= 30){
-            System.out.println("Можно идти гулять");
-        } else if (age <= 20 && temperature >= 0 && temperature <= 28){
-            System.out.println("Можно идти гулять");
+        if (age >= 20 && age <=45 && temperature >= -20 && temperature <= 30){
+            return  "Можно идти гулять";
+        } else if (age < 20 && temperature >= 0 && temperature <= 28){
+            return  "Можно идти гулять";
         } else if (age >= 45 && temperature >= -10 && temperature <= 25) {
-            System.out.println("Можно идти гулять");
+            return  "Можно идти гулять";
         } else {
-            System.out.println("Оставайся дома");
             return "Оставайся дома";
         }
-        return "Можно идти гулять";
     }
 }
